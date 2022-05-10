3347f619a10b1f63c34a7ca3e06202e59666293a
Mariah <mariahschock@gmail.com>
2022-05-10 14:41:56 -0700

    styling home page

--- cat.data.js
+++ cat.data.js
@@ -6,6 +6,18 @@ export const cats = [
         likes: 'aggressive cuddling',
         // image: './assets/Leo.png',
     },
+    {
+        id: '2',
+        name: 'TK, (Thicc Kitty)',
+        age: 3,
+        likes: 'carbs',
+    },
+    {
+        id: '3',
+        name: 'Phoebe',
+        age: 1,
+        likes: 'to wake up and choose violence',
+    }
     // { name: 'TK, (Thicc Kitty),', age: 3, likes: 'carbs', image: './assets/TK.png' },
     // { name: 'Phoebe', age: 1, likes: 'to wake up and choose violence', image: './assets/Phoebe.png' },
 ];
