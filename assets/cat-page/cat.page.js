4f617d42d75ce147e2772a2ec97e5d98f03fdac3
Mariah <mariahschock@gmail.com>
2022-05-10 15:19:48 -0700

    created new html and js page

---
+++ assets/cat-page/cat.page.js
@@ -0,0 +1,8 @@
+import { cats } from '../cat.data.js';
+import { findById } from '../utils.js';
+
+const params = new URLSearchParams(window.location.search);
+const cat = findById(params.get('id'), cats);
+
+const catName = document.getElementById('cat-name');
+catName.textContent = cat.name;
