9588f5d2e68d72c86b611eda63df099b0fa317be
Mariah <mariahschock@gmail.com>
2022-05-10 16:20:30 -0700

    adding info to catStart

--- catStart/catStart.js
+++ catStart/catStart.js
@@ -7,4 +7,10 @@ const cat = findById(params.get('id'), cats);
 const catName = document.getElementById('cat-name');
 catName.textContent = cat.name;
 
-catName.append;
+const catImg = document.createElement('img');
+catImg.src = `../assets/${cat.name}.png`;
+
+const p = document.createElement('p');
+p.textContent = `${cat.name} is ${cat.age} year(s) old and likes ${cat.likes}`;
+
+catName.append(catImg, p);
