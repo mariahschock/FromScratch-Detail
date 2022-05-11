9043eb3d7427269a4f449e0c8f9c09589a9c8db8
Mariah <mariahschock@gmail.com>
2022-05-10 17:10:47 -0700

    ran test

--- test/example.test.js
+++ test/example.test.js
@@ -1,12 +1,12 @@
 import { renderCat } from '../utils.js';
-import { cats } from '../cat.data.js';
+import { cats } from '../cat-data.js';
 
 const test = QUnit.test;
 
 test('test cat render function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="cat"><h1>Leo</h1><img src="./assets/Leo.png"><p>Leo is 5 year(s) old and likes aggressive cuddling</p></div>`;
+    const expected = `<div class="cat"><a href="./catStart/?id=1"><img src="./assets/Leo.png"><span class="name">Leo</span></a></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
